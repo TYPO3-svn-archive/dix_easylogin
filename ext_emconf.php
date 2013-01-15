b47dbea766a33f53c468587d97cb909b33d78fcd
beko <beko@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-15 19:05:25 +0000

    0.3.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dix_easylogin/trunk@70052 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "dix_easylogin".
-#
-# Auto generated 16-03-2012 14:44
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "dix_easylogin".
+ *
+ * Auto generated 15-01-2013 20:04
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Easy Login and Register with OpenID (FE)',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.2.7',
+	'version' => '0.3.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-4.6.99',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:39:{s:9:"ChangeLog";s:4:"a46b";s:10:"README.txt";s:4:"bfc8";s:20:"class.ext_update.php";s:4:"a4bb";s:33:"class.tx_dixeasylogin_realurl.php";s:4:"96c5";s:21:"ext_conf_template.txt";s:4:"2e72";s:12:"ext_icon.gif";s:4:"f9b3";s:17:"ext_localconf.php";s:4:"a778";s:14:"ext_tables.php";s:4:"66b1";s:14:"ext_tables.sql";s:4:"08e3";s:16:"locallang_db.xml";s:4:"ff17";s:14:"doc/manual.sxw";s:4:"0802";s:33:"pi1/class.tx_dixeasylogin_div.php";s:4:"1a8e";s:38:"pi1/class.tx_dixeasylogin_facebook.php";s:4:"a26a";s:36:"pi1/class.tx_dixeasylogin_oauth1.php";s:4:"730c";s:36:"pi1/class.tx_dixeasylogin_openid.php";s:4:"6acd";s:33:"pi1/class.tx_dixeasylogin_pi1.php";s:4:"dbf3";s:17:"pi1/locallang.xml";s:4:"ece3";s:29:"res/dope/class.dopeopenid.php";s:4:"d654";s:21:"res/icons/blogger.ico";s:4:"6c92";s:22:"res/icons/facebook.jpg";s:4:"d707";s:20:"res/icons/flickr.ico";s:4:"9bac";s:20:"res/icons/google.gif";s:4:"ca68";s:22:"res/icons/myopenid.ico";s:4:"b22b";s:21:"res/icons/twitter.gif";s:4:"a8b3";s:23:"res/icons/wordpress.ico";s:4:"6cec";s:19:"res/icons/yahoo.ico";s:4:"1698";s:19:"res/oauth/OAuth.php";s:4:"ee05";s:25:"res/yadis/HTTPFetcher.php";s:4:"5138";s:21:"res/yadis/Manager.php";s:4:"5d0a";s:33:"res/yadis/ParanoidHTTPFetcher.php";s:4:"4489";s:23:"res/yadis/ParseHTML.php";s:4:"965f";s:30:"res/yadis/PlainHTTPFetcher.php";s:4:"09c9";s:17:"res/yadis/XML.php";s:4:"09c4";s:18:"res/yadis/XRDS.php";s:4:"bfe5";s:19:"res/yadis/Yadis.php";s:4:"7567";s:20:"static/constants.txt";s:4:"e9b9";s:16:"static/setup.txt";s:4:"d230";s:20:"templates/login.tmpl";s:4:"b9f4";s:19:"templates/xrds.tmpl";s:4:"adfe";}',
+	'_md5_values_when_last_written' => 'a:42:{s:9:"ChangeLog";s:4:"33eb";s:20:"class.ext_update.php";s:4:"17d5";s:33:"class.tx_dixeasylogin_realurl.php";s:4:"96c5";s:21:"ext_conf_template.txt";s:4:"396d";s:12:"ext_icon.gif";s:4:"f9b3";s:17:"ext_localconf.php";s:4:"a778";s:14:"ext_tables.php";s:4:"b45b";s:14:"ext_tables.sql";s:4:"dcb1";s:36:"icon_tx_dixeasylogin_identifiers.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"1f59";s:10:"README.txt";s:4:"e88b";s:7:"tca.php";s:4:"41bf";s:14:"doc/manual.sxw";s:4:"3462";s:33:"pi1/class.tx_dixeasylogin_div.php";s:4:"c1d2";s:38:"pi1/class.tx_dixeasylogin_facebook.php";s:4:"a26a";s:36:"pi1/class.tx_dixeasylogin_oauth1.php";s:4:"05fc";s:36:"pi1/class.tx_dixeasylogin_openid.php";s:4:"6acd";s:33:"pi1/class.tx_dixeasylogin_pi1.php";s:4:"bf0b";s:17:"pi1/locallang.xml";s:4:"ece3";s:29:"res/dope/class.dopeopenid.php";s:4:"d654";s:21:"res/icons/blogger.ico";s:4:"6c92";s:22:"res/icons/facebook.jpg";s:4:"d707";s:20:"res/icons/flickr.ico";s:4:"9bac";s:20:"res/icons/google.gif";s:4:"ca68";s:22:"res/icons/myopenid.ico";s:4:"b22b";s:21:"res/icons/twitter.gif";s:4:"a8b3";s:23:"res/icons/wordpress.ico";s:4:"6cec";s:18:"res/icons/xing.png";s:4:"b84d";s:19:"res/icons/yahoo.ico";s:4:"1698";s:19:"res/oauth/OAuth.php";s:4:"ee05";s:25:"res/yadis/HTTPFetcher.php";s:4:"5138";s:21:"res/yadis/Manager.php";s:4:"5d0a";s:33:"res/yadis/ParanoidHTTPFetcher.php";s:4:"4489";s:23:"res/yadis/ParseHTML.php";s:4:"965f";s:30:"res/yadis/PlainHTTPFetcher.php";s:4:"09c9";s:17:"res/yadis/XML.php";s:4:"09c4";s:18:"res/yadis/XRDS.php";s:4:"bfe5";s:19:"res/yadis/Yadis.php";s:4:"7567";s:20:"static/constants.txt";s:4:"f70a";s:16:"static/setup.txt";s:4:"4996";s:20:"templates/login.tmpl";s:4:"0c13";s:19:"templates/xrds.tmpl";s:4:"adfe";}',
 	'suggests' => array(
 	),
 );
